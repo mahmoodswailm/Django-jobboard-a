17b6e9b7bc3755b1909388c3e337679437f42531
mahmoodswailm <mahmood.swailm@gmail.com>
2025-08-03 16:12:30 +0300

    add pagination to jobs.html

--- job/views.py
+++ job/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Job    
+from django.core.paginator import Paginator
+
+
 # Create your views here.
 
 def job_list(request):
-    jobs =Job.objects.all
+    jobs =Job.objects.all()
+    p = Paginator(jobs,1)  # Show 1 jobs per page (pagination object)
+    page_num= request.GET.get("page")
+    page_obj = p.get_page(page_num) 
+    # page_obj is the current page of jobs
     return render(request,"job\jobs.html", context={
-        "jobs":jobs
+        "page_obj":page_obj,"jobs":jobs
     })
 
 def job_details(request,id):
@@ -15,3 +22,44 @@ def job_details(request,id):
                 context={
         "job_de":job
     })
+    
+    
+    
+    
+    
+# Example usage of Paginator
+    
+# p.count
+# 4
+# >>> p.num_pages
+# 2
+# >>> type(p.page_range)
+# <class 'range_iterator'>
+# >>> p.page_range
+# range(1, 3)
+
+# >>> page1 = p.page(1)
+# >>> page1
+# <Page 1 of 2>
+# >>> page1.object_list
+# ['john', 'paul']
+
+# >>> page2 = p.page(2)
+# >>> page2.object_list
+# ['george', 'ringo']
+# >>> page2.has_next()
+# False
+# >>> page2.has_previous()
+# True
+# >>> page2.has_other_pages()
+# True
+# >>> page2.next_page_number()
+# Traceback (most recent call last):
+# ...
+# EmptyPage: That page contains no results
+# >>> page2.previous_page_number()
+# 1
+# >>> page2.start_index()  # The 1-based index of the first item on this page
+# 3
+# >>> page2.end_index()  # The 1-based index of the last item on this page
+# 4
